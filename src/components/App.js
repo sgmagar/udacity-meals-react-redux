51d037780a7912fd82c2ff16c07c4319327105ad
Saurav <sp.gharti@gmail.com>
2017-08-31 15:45:23 +0545

    create shoppinglist

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,16 @@ import ArrowRightIcon from "react-icons/lib/fa/arrow-circle-right";
 import Loading from "react-loading";
 import { fetchRecipes } from "../utils/api";
 import FoodList from "./FoodList";
+import ShoppingList from "./ShoppingList";
 
 class App extends Component {
   state = {
     foodModalOpen: false,
     meal: null,
     day: null,
-    food: null
+    food: null,
+    ingredientsModalOpen: false,
+    loadingFood: false
   };
   openFoodModal = ({ meal, day }) => {
     this.setState(() => ({
@@ -47,13 +50,42 @@ class App extends Component {
       }))
     );
   };
+  openIngredientsModal = () =>
+    this.setState(() => ({ ingredientsModalOpen: true }));
+  closeIngredientsModal = () =>
+    this.setState(() => ({ ingredientsModalOpen: false }));
+  generateShoppingList = () => {
+    return this.props.calendar
+      .reduce((result, { meals }) => {
+        const { breakfast, lunch, dinner } = meals;
+
+        breakfast && result.push(breakfast);
+        lunch && result.push(lunch);
+        dinner && result.push(dinner);
+
+        return result;
+      }, [])
+      .reduce((ings, { ingredientLines }) => ings.concat(ingredientLines), []);
+  };
   render() {
-    const { foodModalOpen, loadingFood, food } = this.state;
+    const {
+      foodModalOpen,
+      loadingFood,
+      food,
+      ingredientsModalOpen
+    } = this.state;
     const { calendar, selectRecipe, remove } = this.props;
     const mealOrder = ["breakfast", "lunch", "dinner"];
 
     return (
       <div className="container">
+        <div className="nav">
+          <h1 className="header">UdaciMeals</h1>
+          <button className="shopping-list" onClick={this.openIngredientsModal}>
+            Shopping List
+          </button>
+        </div>
+
         <ul className="meal-types">
           {mealOrder.map(mealType =>
             <li key={mealType} className="subheader">
@@ -143,6 +175,17 @@ class App extends Component {
                     />}
                 </div>}
           </div>
+        </Modal>
+
+        <Modal
+          className="modal"
+          overlayClassName="overlay"
+          isOpen={ingredientsModalOpen}
+          onRequestClose={this.closeIngredientsModal}
+          contentLabel="Modal"
+        >
+          {ingredientsModalOpen &&
+            <ShoppingList list={this.generateShoppingList()} />}
         </Modal>
       </div>
     );
